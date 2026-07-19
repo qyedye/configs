291ab7edcb62c8aa9c44eac952b595e416abd46a
Mikhail Kobzar <mishablackops@gmail.com>
2026-07-19 21:33:04 +0500

    fish: add config files for fish shell (main shell)

---
+++ fish/config.fish
@@ -0,0 +1,29 @@
+if status is-interactive
+    abbr ls "eza --group-directories-first --sort=name"
+    abbr ll "eza -lh --group-directories-first --sort=name"
+    abbr la "eza -lha --group-directories-first --sort=name"
+    abbr tree "lstr --dirs-first --dotfiles-first --natural-sort"
+end
+
+function fish_prompt
+    set -l user (whoami)
+    set -l host (hostname -s)
+    set -l dir (prompt_pwd)
+    set -l git_branch (fish_git_prompt)
+
+    echo -n (set_color green)"$user"
+    echo -n (set_color normal)" at "
+    echo -n (set_color green)"$host"
+    echo -n (set_color blue)" [$dir]"
+    
+    if test -n "$git_branch"
+        echo -n (set_color magenta)"$git_branch"
+    end
+    
+    echo -n (set_color normal)
+    echo
+    echo -n (set_color normal)"λ "
+end
+
+set -g fish_autosuggestion_enabled 0
+set -g fish_greeting ""
